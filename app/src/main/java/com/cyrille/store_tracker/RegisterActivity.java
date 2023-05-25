a5021d72050abff0cc7843f76cd64796571abd13
cyrille <cyrillekyrillos6@gmail.com>
2023-05-25 20:55:58 +0000

    Premier commit

---
+++ app/src/main/java/com/cyrille/store_tracker/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.cyrille.store_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    TextView bien;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
